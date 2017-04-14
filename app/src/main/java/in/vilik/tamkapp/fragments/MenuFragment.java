11f0975312da148fa7a4ed03bd21f6d91755d003
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-14 21:47:55 +0300

    Move meal options in line with other items.

--- app/src/main/java/in/vilik/tamkapp/fragments/MenuFragment.java
+++ app/src/main/java/in/vilik/tamkapp/fragments/MenuFragment.java
@@ -2,6 +2,8 @@ package in.vilik.tamkapp.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
