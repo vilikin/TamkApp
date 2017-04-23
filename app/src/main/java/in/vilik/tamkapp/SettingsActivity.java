06e5cceb9a4c856658d9fe95ee7d130a77864709
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-23 17:56:54 +0300

    Fix recreation of main activity after coming back from settings.

--- app/src/main/java/in/vilik/tamkapp/SettingsActivity.java
+++ app/src/main/java/in/vilik/tamkapp/SettingsActivity.java
@@ -29,7 +29,7 @@ public class SettingsActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                finish();
                 return true;
         }
 
