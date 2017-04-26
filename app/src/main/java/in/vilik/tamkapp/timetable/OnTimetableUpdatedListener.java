0067f243add00f2793a9f515ed5058bd5137c970
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 16:28:58 +0300

    Write javadoc for OnTimetableUpdatedListener.

--- app/src/main/java/in/vilik/tamkapp/timetable/OnTimetableUpdatedListener.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/OnTimetableUpdatedListener.java
@@ -1,10 +1,21 @@
 package in.vilik.tamkapp.timetable;
 
 /**
- * Created by vili on 16/04/2017.
+ * Defines an interface for timetable update listeners.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public interface OnTimetableUpdatedListener {
+
+    /**
+     * Triggers when update was successful.
+     */
     void onSuccess();
+
+    /**
+     * Triggers when update was unsuccessful.
+     */
     void onError();
 }
