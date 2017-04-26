dcde9bfb9d9ca6df0031db85d41af96bb556a2fc
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 12:26:07 +0300

    Write Javadoc for DataLoader.

--- app/src/main/java/in/vilik/tamkapp/utils/DataLoader.java
+++ app/src/main/java/in/vilik/tamkapp/utils/DataLoader.java
@@ -15,61 +15,136 @@ import okhttp3.Response;
  * Created by vili on 16/04/2017.
  */
 
+/**
+ * Implements a base class for all API handlers.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
+ */
 public abstract class DataLoader {
-    enum LoadingStatus {
-        SUCCESSFULLY_LOADED, LOADING_ERROR, LOADING_IN_PROGRESS
-    }
 
+    /**
+     * Defines all available data loading strategies that can be used.
+     *
+     * CACHE_FIRST: Tries to read cache first, if unavailable, tries server
+     * SERVER_FIRST: Tries to load from server first, if unavailable, tries cache
+     * CACHE_ONLY: Only tries to read data from cache
+     * SERVER_ONLY: Only tries to retrieve data from server
+     */
     public enum LoadingStrategy {
         CACHE_FIRST, SERVER_FIRST, CACHE_ONLY, SERVER_ONLY
     }
 
+    /**
+     * Default time in milliseconds cached files should be valid.
+     */
     private final long DEFAULT_CACHE_MAX_AGE = 1000 * 60 * 60 * 24;
 
-    private API api;
-    private LoadingStatus status;
-    private OnDataLoadedListener listener;
+    /**
+     * Time in milliseconds cached files should be valid.
+     */
     private long cacheMaxAge = DEFAULT_CACHE_MAX_AGE;
 
+    /**
+     * Defines all details that are needed for connecting to the specified API.
+     */
+    private API api;
+
+    /**
+     * Listener that is called when data is loaded or error occures.
+     */
+    private OnDataLoadedListener listener;
+
+    /**
+     * Context.
+     */
     private Context context;
 
+    /**
+     * Method prefix used for debugging purposes to identify which kind of API this is.
+     */
     private String methodPrefix;
 
+    /**
+     * Initializes context of the loader.
+     *
+     * @param context  Context for the loader
+     */
     public DataLoader(Context context) {
         this.context = context;
     }
 
+    /**
+     * Initializes context and API details for the loader.
+     *
+     * @param context   Context for the loader
+     * @param api       API to interact with
+     */
     public DataLoader(Context context, API api) {
         this.context = context;
 
         setApi(api);
     }
 
+    /**
+     * Gets API details.
+     *
+     * @return  API details
+     */
     public API getApi() {
         return api;
     }
 
+    /**
+     * Sets API details for the loader.
+     *
+     * @param api   API details
+     */
     public void setApi(API api) {
         this.api = api;
         this.methodPrefix = api.getType().name() + ":";
     }
 
+    /**
+     * Gets max age of the cache in milliseconds.
+     *
+     * @return Max age of the cache in milliseconds
+     */
     public long getCacheMaxAge() {
         return cacheMaxAge;
     }
 
+    /**
+     * Sets max age for the cache in milliseconds.
+     *
+     * @param cacheMaxAge   Max age for the cache in milliseconds
+     */
     public void setCacheMaxAge(long cacheMaxAge) {
         this.cacheMaxAge = cacheMaxAge;
     }
 
+    /**
+     * Sets listener for when the loading is completed, either successfully or unsuccessfully.
+     *
+     * @param listener  Listener to call when data is loaded
+     */
     public void setOnDataLoadedListener(OnDataLoadedListener listener) {
         this.listener = listener;
     }
 
+    /**
+     * Removes data loaded listener that is attached to this DataLoader.
+     */
     public void removeOnDataLoadedListener() {
         this.listener = null;
     }
 
+    /**
+     * Starts loading of data based on the specified strategy.
+     *
+     * @param strategy Which strategy should be used to load data
+     */
     public void loadData(LoadingStrategy strategy) {
         switch (strategy) {
             case CACHE_FIRST:
@@ -86,6 +161,11 @@ public abstract class DataLoader {
         }
     }
 
+    /**
+     * Loads data from the API and calls listener when completed. Saves loaded data to cache.
+     *
+     * @param tryCacheOnFailure If enabled, after unsuccessful request cache is used
+     */
     private void loadDataFromServer(final boolean tryCacheOnFailure) {
         OkHttpClient client = new OkHttpClient();
 
@@ -123,6 +203,11 @@ public abstract class DataLoader {
         });
     }
 
+    /**
+     * Reads data from cache and call listener when completed.
+     *
+     * @param tryServerOnFailure If enabled, tries to load data from server when cache is unavailable.
+     */
     private void loadDataFromCache(boolean tryServerOnFailure) {
         if (DataCache.has(context, api.getCacheKey(), cacheMaxAge)) {
             Debug.log(methodPrefix + "loadDataFromCache()", "Cache available, reading data");
