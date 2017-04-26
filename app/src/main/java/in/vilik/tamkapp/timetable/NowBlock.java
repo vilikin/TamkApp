ba8ad00cf63c1ef8a918b742570cfc99baff5379
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 16:27:06 +0300

    Write javadoc for NowBlock.

--- app/src/main/java/in/vilik/tamkapp/timetable/NowBlock.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/NowBlock.java
@@ -1,21 +1,45 @@
 package in.vilik.tamkapp.timetable;
 
-
 /**
- * Created by vili on 16/04/2017.
+ * Implements now block component for timetable.
+ *
+ * Now block contains one reservation that is currently
+ * active and displays it highlighted over everything else.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class NowBlock implements TimetableElement {
+
+    /**
+     * Reservation displayed on the now block.
+     */
     private Reservation reservation;
 
+    /**
+     * Initializes now block with a reservation.
+     *
+     * @param reservation   Reservation to display in the now block
+     */
     public NowBlock(Reservation reservation) {
         this.reservation = reservation;
     }
 
+    /**
+     * Gets reservation of the now block.
+     *
+     * @return Reservation of the now block
+     */
     public Reservation getReservation() {
         return reservation;
     }
 
+    /**
+     * Sets reservation for the now block.
+     *
+     * @param reservation Reservation for the now block
+     */
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
     }
