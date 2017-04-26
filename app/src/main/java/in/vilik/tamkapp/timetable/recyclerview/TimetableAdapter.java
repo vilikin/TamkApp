288b3bb395cef290de12f853806da10f314f3afc
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 14:10:05 +0300

    Write javadoc for TimetableAdapter.

--- app/src/main/java/in/vilik/tamkapp/timetable/recyclerview/TimetableAdapter.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/recyclerview/TimetableAdapter.java
@@ -10,21 +10,46 @@ import in.vilik.tamkapp.timetable.Timetable;
 import in.vilik.tamkapp.timetable.TimetableElement;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements adapter for RecyclerView that holds the timetable.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
 public class TimetableAdapter extends RecyclerView.Adapter<TimetableViewHolder>{
+
+    /**
+     * Timetable instance that is rendered by the RecyclerView.
+     */
     private Timetable timetable;
 
+    /**
+     * Initializes adapter with the timetable instance.
+     *
+     * @param timetable Timetable instance
+     */
     public TimetableAdapter(Timetable timetable) {
         this.timetable = timetable;
     }
 
+    /**
+     * Returns type for a view in specified position.
+     *
+     * @param position  Position of the view
+     * @return          Type for the view in specified position
+     */
     @Override
     public int getItemViewType(int position) {
-
         return timetable.getElements().get(position).getType().ordinal();
     }
 
+    /**
+     * Creates correct view holder specific view type.
+     *
+     * @param parent    Parent ViewGroup
+     * @param viewType  Type of the view
+     * @return          ViewHolder for specified view type
+     */
     @Override
     public TimetableViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
@@ -54,15 +79,24 @@ public class TimetableAdapter extends RecyclerView.Adapter<TimetableViewHolder>{
         return null;
     }
 
+    /**
+     * Binds data to a view holder in specified position.
+     *
+     * @param holder    ViewHolder to bind data to
+     * @param position  Position of the ViewHolder
+     */
     @Override
     public void onBindViewHolder(TimetableViewHolder holder, int position) {
         holder.bind(timetable.getElements().get(position));
     }
 
+    /**
+     * Gets total number of items in the timetable.
+     *
+     * @return  Total number of items in the timetable
+     */
     @Override
     public int getItemCount() {
         return timetable.getElements().size();
     }
-
-
 }
