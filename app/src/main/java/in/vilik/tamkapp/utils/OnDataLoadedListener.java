0c0624ecaada0bff64589b9d57210a59ba4c3bb5
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 13:09:58 +0300

    Write javadoc for OnDataLoadedListener.

--- app/src/main/java/in/vilik/tamkapp/utils/OnDataLoadedListener.java
+++ app/src/main/java/in/vilik/tamkapp/utils/OnDataLoadedListener.java
@@ -1,10 +1,23 @@
 package in.vilik.tamkapp.utils;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements an interface for listening events of DataLoader.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public interface OnDataLoadedListener {
+
+    /**
+     * Triggers when data was loaded successfully.
+     *
+     * @param data Response string
+     */
     void onSuccess(String data);
+
+    /**
+     * Triggers when data could not be loaded.
+     */
     void onFailure();
 }
