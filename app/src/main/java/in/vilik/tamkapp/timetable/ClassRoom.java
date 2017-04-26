cb57a0f96503061aaae79c7a5c69d43fb4391248
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 16:11:32 +0300

    Write javadoc for ClassRoom and Realization.

--- app/src/main/java/in/vilik/tamkapp/timetable/ClassRoom.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/ClassRoom.java
@@ -1,34 +1,79 @@
 package in.vilik.tamkapp.timetable;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements a class representing class room details.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class ClassRoom {
+
+    /**
+     * Code of the class room, e.g. C3-21.
+     */
     private String code;
+
+    /**
+     * Name of the class room, e.g. Game Lab.
+     */
     private String name;
+
+    /**
+     * Name of the building the class room is located in.
+     */
     private String building;
 
+    /**
+     * Gets code of the class room.
+     *
+     * @return  Code of the class room
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * Sets code for the class room.
+     *
+     * @param code  Code for the class room
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     * Gets name of the class room.
+     *
+     * @return Name of the class room
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name for the class room.
+     *
+     * @param name Name for the class room
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets building of the class room.
+     *
+     * @return  Name of the class room
+     */
     public String getBuilding() {
         return building;
     }
 
+    /**
+     * Sets building for the class room.
+     *
+     * @param building  Building for the class room
+     */
     public void setBuilding(String building) {
         this.building = building;
     }
--- app/src/main/java/in/vilik/tamkapp/timetable/Realization.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/Realization.java
@@ -1,25 +1,56 @@
 package in.vilik.tamkapp.timetable;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements a class representing realization details.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class Realization {
+
+    /**
+     * Name of the realization.
+     */
     private String name;
+
+    /**
+     * Code of the realization.
+     */
     private String code;
 
+    /**
+     * Gets name of the realization.
+     *
+     * @return  Name of the realization
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name for the realization.
+     *
+     * @param name  Name for the realization
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets code of the realization.
+     *
+     * @return  Code of the realization
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * Sets code for the realization.
+     *
+     * @param code Code for the realization
+     */
     public void setCode(String code) {
         this.code = code;
     }
