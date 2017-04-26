2720c0b6ee2e917e09140bbc26109dd60e552cd9
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-26 16:22:58 +0300

    Write javadoc for Day and EmptyDay.

--- app/src/main/java/in/vilik/tamkapp/timetable/Day.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/Day.java
@@ -8,41 +8,96 @@ import in.vilik.tamkapp.timetable.notes.Note;
 import in.vilik.tamkapp.utils.DateUtil;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements day component for timetable.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class Day implements TimetableElement {
+
+    /**
+     * Parent timetable the day is contained in.
+     */
     private Timetable parent;
+
+    /**
+     * Date object of the day.
+     */
     private Date date;
+
+    /**
+     * List of reservations associated with the day.
+     */
     private List<Reservation> reservations;
+
+    /**
+     * List of notes associated with the day.
+     */
     private List<Note> notes;
 
+    /**
+     * Initializes day with empty lists of reservations and notes.
+     *
+     * @param parent Parent timetable the day is contained in.
+     */
     public Day(Timetable parent) {
         this.parent = parent;
         this.reservations = new ArrayList<>();
         this.notes = new ArrayList<>();
     }
 
+    /**
+     * Gets parent timetable of the day.
+     *
+     * @return Parent timetable of the day
+     */
     public Timetable getParent() {
         return parent;
     }
 
+    /**
+     * Gets Date object of the day.
+     *
+     * @return  Date object of the day
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Gets literal format of the day.
+     *
+     * @param format    Format to be used by the DateUtil
+     * @return          Formatted date
+     */
     public String getFormattedDate(DateUtil.DateFormat format) {
         return DateUtil.formatDate(parent.getContext(), date, format);
     }
 
+    /**
+     * Sets Date object for the day.
+     *
+     * @param date  Date object for the day
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Gets list of reservations associated with the day.
+     *
+     * @return  List of reservation associated with the day
+     */
     public List<Reservation> getReservations() {
         return reservations;
     }
 
+    /**
+     * Gets list of notes associated with the day.
+     *
+     * @return List of notes associated with the day
+     */
     public List<Note> getNotes() {
         return notes;
     }
--- app/src/main/java/in/vilik/tamkapp/timetable/EmptyDay.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/EmptyDay.java
@@ -6,16 +6,31 @@ import in.vilik.tamkapp.R;
 import in.vilik.tamkapp.utils.DateUtil;
 
 /**
- * Created by vili on 18/04/2017.
+ * Implements day component for timetable that does not have any reservations.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class EmptyDay extends Day {
+
+    /**
+     * Initializes component based on an existing day instance.
+     *
+     * @param day   Day that has no reservations
+     */
     public EmptyDay(Day day) {
         super(day.getParent());
 
         setDate(day.getDate());
     }
 
+    /**
+     * Gets literal format of the day with a note that there are no lectures.
+     *
+     * @param format    Format to be used by the DateUtil
+     * @return          Formatted date
+     */
     @Override
     public String getFormattedDate(DateUtil.DateFormat format) {
         Context context = getParent().getContext();
