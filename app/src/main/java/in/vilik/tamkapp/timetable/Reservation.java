ab503e9dbf6f3b12fb8ca9128f593e99b14986e2
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-04-30 20:00:55 +0300

    Write javadoc for reservation.

--- app/src/main/java/in/vilik/tamkapp/timetable/Reservation.java
+++ app/src/main/java/in/vilik/tamkapp/timetable/Reservation.java
@@ -7,102 +7,234 @@ import java.util.List;
 import in.vilik.tamkapp.utils.DateUtil;
 
 /**
- * Created by vili on 16/04/2017.
+ * Implements a reservation component for timetable.
+ *
+ * @author Vili Kinnunen vili.kinnunen@cs.tamk.fi
+ * @version 2017.0426
+ * @since 1.7
  */
-
 public class Reservation implements TimetableElement {
+
+    /**
+     * ID of the reservation.
+     */
     private int id;
+
+    /**
+     * Day component that holds the reservation.
+     */
     private Day parent;
+
+    /**
+     * Subject of the reservation.
+     */
     private String subject;
+
+    /**
+     * Description of the reservation.
+     */
     private String description;
+
+    /**
+     * Start date of the reservation.
+     */
     private Date startDate;
+
+    /**
+     * End date of the reservation.
+     */
     private Date endDate;
 
+    /**
+     * Class room of the reservation.
+     */
     private ClassRoom classRoom;
+
+    /**
+     * List of realizations.
+     */
     private List<Realization> realizations;
+
+    /**
+     * List of student groups associated with this reservation.
+     */
     private List<String> studentGroups;
 
-    //private Context context;
-
+    /**
+     * Initializes reservation with empty lists of realizations and student groups.
+     */
     public Reservation() {
         this.realizations = new ArrayList<>();
         this.studentGroups = new ArrayList<>();
-
-        //context = getParent().getParent().getContext();
     }
 
+    /**
+     * Gets id of the reservation.
+     *
+     * @return Id of the reservation
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id for the reservation.
+     *
+     * @param id Id for the reservation
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets parent timetable of the reservation.
+     *
+     * @return  Parent timetable of the reservation
+     */
     public Day getParent() {
         return parent;
     }
 
+    /**
+     * Sets parent timetable for the reservation.
+     *
+     * @param parent Sets parent timetable for the reservation
+     */
     public void setParent(Day parent) {
         this.parent = parent;
     }
 
+    /**
+     * Gets subject of the reservation.
+     *
+     * @return Subject of the reservation
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * Sets subject for the reservation.
+     *
+     * @param subject Subject for the reservation
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    /**
+     * Gets description of the reservation.
+     *
+     * @return Description of the reservation
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description for the reservation.
+     *
+     * @param description Description for the reservation
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets start date of the reservation.
+     *
+     * @return Start date of the reservation
+     */
     public Date getStartDate() {
         return startDate;
     }
 
+    /**
+     * Sets start date for the reservation.
+     *
+     * @param startDate Start date for the reservation
+     */
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * Gets end date of the reservation.
+     *
+     * @return End date of the reservation
+     */
     public Date getEndDate() {
         return endDate;
     }
 
+    /**
+     * Sets end date for the reservation.
+     *
+     * @param endDate End date for the reservation
+     */
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
 
+    /**
+     * Gets class room of the reservation.
+     *
+     * @return Class room for the reservation
+     */
     public ClassRoom getClassRoom() {
         return classRoom;
     }
 
+    /**
+     * Sets class room for the reservation.
+     *
+     * @param classRoom Class room for the reservation
+     */
     public void setClassRoom(ClassRoom classRoom) {
         this.classRoom = classRoom;
     }
 
+    /**
+     * Gets realizations associated with the reservation.
+     *
+     * @return Realizations associated with the reservation
+     */
     public List<Realization> getRealizations() {
         return realizations;
     }
 
+    /**
+     * Sets realization associated with the reservation.
+     *
+     * @param realizations Realizations associated with the reservation
+     */
     public void setRealizations(List<Realization> realizations) {
         this.realizations = realizations;
     }
 
+    /**
+     * Gets student groups associated with the reservation.
+     *
+     * @return Student groups associated with the reservation
+     */
     public List<String> getStudentGroups() {
         return studentGroups;
     }
 
+    /**
+     * Sets student groups associated with the reservation.
+     *
+     * @param studentGroups Student groups associated with the reservation
+     */
     public void setStudentGroups(List<String> studentGroups) {
         this.studentGroups = studentGroups;
     }
 
+    /**
+     * Constructs header of the reservation for the Recycler View.
+     *
+     * @return  Header of the reservation
+     */
     public String getViewHeader() {
         if (subjectHasRealizationString()) {
             StringBuilder sb = new StringBuilder();
@@ -119,6 +251,11 @@ public class Reservation implements TimetableElement {
         }
     }
 
+    /**
+     * Concatenates all realization codes associated with the reservation into one string.
+     *
+     * @return All realization codes associated with the reservation comma separated
+     */
     public String getRealizationsString() {
         StringBuilder sb = new StringBuilder();
 
@@ -129,6 +266,11 @@ public class Reservation implements TimetableElement {
         return sb.toString().substring(2);
     }
 
+    /**
+     * Concatenates all student groups associated with the reservation into one string.
+     *
+     * @return All student groups associated with the reservation comma separated
+     */
     public String getStudentGroupsString() {
         StringBuilder sb = new StringBuilder();
 
@@ -139,6 +281,11 @@ public class Reservation implements TimetableElement {
         return sb.toString().substring(2);
     }
 
+    /**
+     * Constructs time period string for the Recycler View.
+     *
+     * @return  Time period string
+     */
     public String getViewDateString() {
         String start = DateUtil.getDigitalTime(startDate);
         String end = DateUtil.getDigitalTime(endDate);
@@ -146,6 +293,11 @@ public class Reservation implements TimetableElement {
         return start + " - " + end;
     }
 
+    /**
+     * Checks if subject of the reservation contains realization string.
+     *
+     * @return  If subject contains realization string or not
+     */
     private boolean subjectHasRealizationString() {
         if (realizations.size() == 0) {
             return false;
